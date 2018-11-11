208f76e5c69bd935cc04ca184abd6d90eee16c84
Raphael Guntersweiler <raphael@rgunti.ch>
2018-11-11 22:58:38 +0100

    Project ready to go

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+CARPI REDIS DATA BUS
+(C) 2018, Raphael "rGunti" Guntersweiler
+Licensed under MIT
+"""
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='carpi-gpsdaemon',
+      version='0.1.0',
+      description='GPS daemon written in Python transmitting from GPSD to a Redis Data Bus',
+      long_description=long_description,
+      url='https://github.com/rGunti/CarPi-GPSDaemon',
+      keywords='carpi gps daemon',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6'
+      ],
+      author='Raphael "rGunti" Guntersweiler',
+      author_email='raphael@rgunti.ch',
+      license='MIT',
+      packages=['gpsdaemon'],
+      install_requires=[
+          'wheel',
+          'carpi-redisdatabus'
+      ],
+      zip_safe=False,
+      include_package_data=True)
